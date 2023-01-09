e75f14ccf9cfd456c4c88ed2062f93e0b897a78e
Nabeel Akbar <studiosnabeel@gmail.com>
2023-01-09 20:35:27 +0500

    Routes created in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
 import React from 'react';
+import './App.css';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import DetailsPage from './components/DetailsPage';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/DetailsPage" element={<DetailsPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
