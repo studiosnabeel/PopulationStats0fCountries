b37ae6ad82146f0e6ec1b3e6f64c462f23c01c48
Nabeel Akbar <studiosnabeel@gmail.com>
2023-01-10 20:44:02 +0500

    Add Homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import api from '../redux/api';
+import { IconName } from 'react-icons/fa';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const dispatch = useDispatch();
+  const countries = useSelector((state) => state.countries.countries);
+
+  const [searchItem, setSearchItem] = useState('');
+
+  useEffect(() => {
+    if (countries.length === 0) {
+      dispatch(api());
+    }
+  }, [dispatch, countries.length]);
+
+  const selectedCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(searchItem.toLocaleLowerCase()) ||
+      country.region.toLowerCase().includes(searchItem.toLocaleLowerCase())
+  );
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchItem(e.target.value);
+  };
+
+  return (
+    <div>
+      <header className="home-container">
+        <div className="search-logo">
+          <IconName className="search-Icon" />
+        </div>
+        <div className="input-search">
+          {' '}
+          <input
+            type="text"
+            name="searchItem"
+            placeholder="search for a country"
+            value={searchItem}
+            onChange={handleSearch}
+          />
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default HomePage;
